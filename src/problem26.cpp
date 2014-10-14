41266a5ce9f235398ebccaf53c947b974ce5b045
feanor <std.feanor@gmail.com>
2014-10-14 16:00:42 +0300

    Solution to problem 26 no longer relies on gmp library.

--- src/problem26.cpp
+++ src/problem26.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-#include <numeric>
-#include <cmath>
-#include <gmpxx.h>
 #include <primes.h>
+#include <util.h>
 
 size_t reciprocal_cycle_length(size_t prime) {
-	mpz_class p = prime;
-	mpz_class base = 9_mpz;
-
+	size_t base = 9;
 	size_t result = 1;
-	while(base % p != 0_mpz) {
-		base = base*10 + 9;
+	while(base) {
+		base = (base*10 + 9) % prime;
 		++result;
 	}
 
@@ -22,16 +18,18 @@ int main() {
 
 	size_t longest_rcp_cycle_number = 0;
 	size_t longest_rcp_cycle_length = 0;
-	//no need to check 2,3,5 since 2 and 5 are exceptions to our rule for determining reciprocal cycle length
-	for(auto p=primes.rbegin();p!=primes.rend()-3;++p) {
-		//std::cout << "Checking " << *p << std::endl;
-		auto length = reciprocal_cycle_length(*p);
-		if(length > longest_rcp_cycle_length) {
-			longest_rcp_cycle_length = length;
-			longest_rcp_cycle_number = *p;
-		}
-	}
 
+	auto t = measure([&] {
+			//no need to check 2,3,5 since 2 and 5 are exceptions to our rule for determining reciprocal cycle length
+			for(auto p=primes.rbegin();p!=primes.rend()-3;++p) {
+				auto length = reciprocal_cycle_length(*p);
+				if(length > longest_rcp_cycle_length) {
+					longest_rcp_cycle_length = length;
+					longest_rcp_cycle_number = *p;
+				}
+			}
+	});
+	std::cout << "Time elapsed: " << t.count() << std::endl;
 	std::cout << longest_rcp_cycle_number << " [" << longest_rcp_cycle_length << "]" << std::endl;
 
 	return 0;
