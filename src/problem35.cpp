29c7262afdfa2b2e079fda7452d0e521c27ec63e
feanor <std.feanor@gmail.com>
2014-10-17 14:17:58 +0300

    Solution to problem 35

---
+++ src/problem35.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <primes.h>
+
+size_t digit_count(size_t number, size_t base=10) {
+	size_t count = 0;
+	while(number) {
+		++count;
+		number /= base;
+	}
+	return count;
+}
+
+size_t circular_shift(size_t number, size_t digits, size_t base=10) {
+	size_t high = 1;
+	while(--digits) high*=base;
+	return high*(number % base) + number / 10;
+}
+
+bool is_circular(size_t prime, std::vector<bool> presence) {
+	size_t digits = digit_count(prime);
+
+	for(size_t i=0;i<digits-1;i++) {
+		prime = circular_shift(prime,digits);
+		if(prime % 2 == 0) return false;
+		if(!presence[prime/2 - 1]) return false;
+	}
+
+	return true;
+}
+
+int main() {
+	const auto primes_state = generate_primes_state(1000000);
+	const auto& primes = std::get<0>(primes_state);
+	const auto& presence = std::get<1>(primes_state);
+
+	size_t count = 0;
+
+	for(auto i: primes) {
+		if(is_circular(i,presence)) {
+			++count;
+			std::cout << i << std::endl;
+		}
+	}
+
+	std::cout << "Count: " << count << std::endl;
+
+	return 0;
+}
