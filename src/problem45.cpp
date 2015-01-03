4c7f5e6bc3697a19ce1f9fde089e43255d6a3b5f
feanor <std.feanor@gmail.com>
2015-01-03 17:18:11 +0200

    Added solution to problem 45

---
+++ src/problem45.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <limits>
+#include <util.h>
+#include <cstdint>
+
+uint64_t hexagonal_number(size_t n) {
+	return n*(2*n-1);
+}
+
+int64_t perfect_square_root(int64_t x) {
+	if(x < 0) return -1;
+	if(x < 2) return x;
+	int64_t a = x;
+	int64_t b = x/2;
+	while(a > b) {
+		a = b;
+		b = (a + x/b)/2;
+	}
+	if(b*b == x) {
+		return b;
+	}
+	return -1;
+}
+
+bool is_triangle_number(uint64_t x) {
+	int64_t d = perfect_square_root(8*x+1);
+	if(-1 == d) return false;
+
+	return (d-1)%2 == 0;
+}
+
+bool is_pentagonal_number(uint64_t x) {
+	int64_t d = perfect_square_root(24*x+1);
+	if(-1 == d) return false;
+
+	return (d+1)%6 == 0;
+}
+
+size_t search(size_t start) {
+	for(size_t n=start;n<std::numeric_limits<size_t>::max();n++) {
+		uint64_t h = hexagonal_number(n);
+		if(is_triangle_number(h) && is_pentagonal_number(h)) {
+			return h;
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	Measure measure;
+	auto number = search(143+1);
+	auto passed = measure.passed();
+	std::cout << "Search for " << number << " took: " << passed << " ms" << std::endl;
+
+	return 0;
+}
