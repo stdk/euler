b99e545b08913d0377a84f549ae93a3f0879371c
feanor <std.feanor@gmail.com>
2015-01-04 15:00:54 +0200

    Solution to problem 48

---
+++ src/problem48.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdint>
+
+uint64_t pow_mod(uint64_t n, size_t p, uint64_t mod) {
+	uint64_t r = 1;
+	while(p--) {
+		r = r*n % mod;
+	}
+	return r;
+}
+
+int main(int argc, char **argv) {
+
+	const uint64_t mod = 10000000000;
+
+	uint64_t s = 0;
+	for(uint32_t i=1;i<=1000;i++) {
+		s = (s + pow_mod(i,i,mod)) % mod;
+	}
+
+	std::cout << s << std::endl;
+
+	return 0;
+}
