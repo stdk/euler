42bbd2ee5b076194b978b47e5ded71f49d302f01
feanor <std.feanor@gmail.com>
2015-01-04 13:05:59 +0200

    Prime presence generation algorithm improved by introducing coalesced branch, where both primes are being processed simultaneously.

--- src/primes.cpp
+++ src/primes.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 std::tuple<const Primes, const PrimePresence> generate_primes_state(const size_t limit) {
-	std::vector<size_t> primes(limit/(log(limit)-1.1));
 
 	// see http://en.wikipedia.org/wiki/Prime-counting_function
 	//primes.reserve(1.25506*limit/log(limit));
 	//primes.reserve(limit/(log(limit)-1.1));
+	std::vector<size_t> primes(limit/(log(limit)-1.1));
 
 	auto prime = primes.begin();
 	*prime++ = 2;
@@ -68,11 +68,70 @@ const PrimePresence generate_primes_presence(const size_t limit) {
 	// only numbers of the following form: 6*n - 1, 6*n + 1, n >= 1 can be primes
 	std::vector<bool> N(limit/3 + 1, true);
 
-	for(size_t i=1; i < limit/6; i++) {
-		if(N[2*i - 2]) {
+	const size_t bound = limit/6;
+
+	for(size_t i=1; i < bound; i++) {
+		const bool a = N[2*i - 2];
+		const bool b = N[2*i - 1];
+		if(a && b) {
+			const size_t p1 = 6*i-1;
+			const size_t p2 = 6*i+1;
+			const size_t upper = bound + i + 1;
+			const size_t lower = bound - i + 1;
+			size_t j1 = p1,j2 = p2;
+			while(j1<lower && j2<lower) {
+				N[(j1-i)*2-1] = false;
+				N[(j1+i-1)*2] = false;
+
+				N[(j2-i-1)*2] = false;
+				N[(j2+i)*2-1] = false;
+
+				j1+=p1;
+				j2+=p2;
+			}
+
+			while(j1<lower && j2<upper) {
+				N[(j1-i)*2-1] = false;
+				N[(j1+i-1)*2] = false;
+
+				N[(j2-i-1)*2] = false;
+
+				j1+=p1;
+				j2+=p2;
+			}
+
+			while(j1<upper && j2<lower) {
+				N[(j1-i)*2-1] = false;
+
+				N[(j2-i-1)*2] = false;
+				N[(j2+i)*2-1] = false;
+
+				j1+=p1;
+				j2+=p2;
+			}
+
+			while(j1<upper && j2 < upper) {
+				N[(j1-i)*2-1] = false;
+
+				N[(j2-i-1)*2] = false;
+
+				j1+=p1;
+				j2+=p2;
+			}
+
+			while(j1<upper) {
+				N[(j1-i)*2-1] = false;
+				j1+=p1;
+			}
+
+			while(j2<upper) {
+				N[(j2-i-1)*2] = false;
+				j2+=p2;
+			}
+		} else if(a) {
 			const size_t p = 6*i-1;
-			const size_t upper = limit/6 + i + 1;
-			const size_t lower = limit/6 - i + 1;
+			const size_t upper = bound + i + 1;
+			const size_t lower = bound - i + 1;
 			size_t j = p;
 			while(j<lower) {
 				N[(j-i)*2-1] = false;
@@ -83,11 +142,10 @@ const PrimePresence generate_primes_presence(const size_t limit) {
 				N[(j-i)*2-1] = false;
 				j+=p;
 			}
-		}
-		if(N[2*i - 1]) {
+		} else if(b) {
 			const size_t p = 6*i+1;
-			const size_t upper = limit/6 + i + 1;
-			const size_t lower = limit/6 - i + 1;
+			const size_t upper = bound + i + 1;
+			const size_t lower = bound - i + 1;
 			size_t j = p;
 			while(j<lower) {
 				N[(j-i-1)*2] = false;
