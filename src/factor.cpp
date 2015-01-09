2fe8f6cfa3b5a823e38c134114cf9bf48c6cd9c8
feanor <std.feanor@gmail.com>
2015-01-09 14:00:17 +0200

    Factorization now requires much less number of primes.

--- src/factor.cpp
+++ src/factor.cpp
@@ -12,31 +12,26 @@ int main(int argc, char **argv) {
 	}
 
 	std::istringstream in(argv[1]);
-	uint32_t number;
+	uint64_t number;
 	in >> number;
 
 	Measure measure;
 
-	const size_t limit = number/2 + 1;
+	const size_t limit = (size_t)sqrt(number)+1;
 	const auto primes_state = generate_primes_state(limit);
 	const auto& primes = std::get<0>(primes_state);
 
 	auto passed = measure.passed();
 	std::cout << "Prime generation up to " << limit << " took: " << passed << " ms" << std::endl;
 
-	CompactFactors factors;
-	if(factorize_compact(number,primes,factors)) {
-		for(auto factor: factors) {
-			for(size_t i=0;i<std::get<1>(factor);i++) {
-				std::cout << std::get<0>(factor) << " ";
-			}
+	auto factors = factorize_compact(number,primes);
+	for(auto factor: factors) {
+		for(size_t i=0;i<std::get<1>(factor);i++) {
+			std::cout << std::get<0>(factor) << " ";
 		}
-		std::cout << std::endl;
-		return 0;
-	} else {
-		std::cout << number << std::endl;
-		return 2;
 	}
+	std::cout << std::endl;
+	return 0;
 
 	return 0;
 }
