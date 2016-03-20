e57c67f7e2369beefacd3ddcaeda59fd77e56f02
feanor <std.feanor@gmail.com>
2016-03-20 19:42:06 +0200

    Added solution to problem 63

---
+++ src/problem63.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <util.h>
+
+/*
+ * The 5-digit number, 16807=7^5, is also a fifth power.
+ * Similarly, the 9-digit number, 134217728=8^9, is a ninth power.
+ * How many n-digit positive integers exist which are also an nth power?
+ *
+ */
+
+size_t powerful_digits_count() {
+	size_t count = 0;
+
+	for(size_t x=0;x<10;x++) {
+		size_t k_max = std::floor(1 / (1 - std::log(x)/std::log(10)));
+		count += k_max;
+	}
+
+	return count;
+}
+
+int main() {
+	Measure measure;
+
+	auto count = powerful_digits_count();
+
+	auto passed = measure.passed();
+	std::cout << "Counting took: " << passed << " ms" << std::endl;
+
+	std::cout << "Powerful digits count: " << count << std::endl;
+
+	return 0;
+}
