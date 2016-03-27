8d4b67948cdcb4a6467c5280628c9c9cf37ae1e8
feanor <std.feanor@gmail.com>
2016-03-27 21:03:38 +0300

    Moved test-related stuff into separate project.

---
+++ test/misc.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <algorithm>
+#include <vector>
+#include <cstdint>
+
+TEST_CASE( "std::rotate", "[std::rotate]" ) {
+	std::vector<int> v  = {1,2,3,4,5,6,7};
+	std::vector<int> v2 = {6,7,1,2,3,4,5};
+
+	std::rotate(v.begin(),v.begin()+5,v.end());
+
+	REQUIRE(v == v2);
+}
