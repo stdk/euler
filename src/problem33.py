14ade8fd2b486dfbadf7317cec36b16fbea8edc5
feanor <std.feanor@gmail.com>
2016-04-09 17:50:10 +0300

    Python solution to problem 33 replaced by cpp one.

--- src/problem33.py
+++
@@ -1,8 +0,0 @@
-f = z*(10*y + x) - y*(10*x+z)
-
-for a in range(0,10):
-    for b in range(0,10):
-        for c in range(0,10):
-            if f.subs(x,a).subs(y,b).subs(z,c) == 0:
-                if a != 0 and b != 0 and c != 0:
-                    print a,b,c
