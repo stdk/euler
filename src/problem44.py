44fdc801e2c3285bf7cca9c02b19ebfb6d365ea4
stdk <std.feanor@gmail.com>
2016-04-11 18:38:13 +0300

    Decreased execution time of problem44 solution

--- src/problem44.py
+++ src/problem44.py
@@ -1,37 +1,20 @@
 def pentagon(n):
     return n*(3*n-1)//2    
 
-def is_perfect_square(x):
-    if x < 0: return False
-    if x < 2: return True
-    a = x
-    b = (a)//2
-    while a > b:
-        a = b
-        b = (a + x//a)//2
-    if b*b == x:
-        return b
-
-def is_pentagon(x):
-    d = is_perfect_square(24*x+1)
-    if d is not None:
-        n,q = divmod(d+1,6)
-        if q==0:
-            return n
-
-def search(max_distance = 1000,max_index = 3000):
+def search(max_distance = 500,max_index = 3000):
+    pentagons = [pentagon(i) for i in xrange(max_index+max_distance)]
+    pentagons_set = frozenset(pentagons)
     for distance in range(1,max_distance):
         for x in range(1,max_index):
             y = x + distance
-            diff = pentagon(x)
-            summ = pentagon(y)
+            diff = pentagons[x]
+            summ = pentagons[y]
+            
             first,remainder = divmod(diff+summ,2)
             if remainder==0:
-                first_index = is_pentagon(first)
-                if first_index is not None:
+                if first in pentagons_set:
                     second = summ - first
-                    second_index = is_pentagon(second)
-                    if second_index is not None:
+                    if second in pentagons_set:
                         return diff
                     
 print search()
