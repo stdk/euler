a2b854cded088b19d7816623df97fed829c485f6
stdk <std.feanor@gmail.com>
2016-05-02 22:25:30 +0300

    Implemented Arg::Unsigned and fixed std::stoul via proxy function.

--- inc/optionparser_customized.h
+++ inc/optionparser_customized.h
@@ -16,11 +16,21 @@ std::ostream& operator<<(std::ostream& os, const option::Option& opt) {
 
 struct Arg: public option::Arg
 {
+    static unsigned long stoul(const std::string& str, std::size_t* pos = 0, int base = 10) {
+        try {
+            return std::stoul(str,pos,base);
+        } catch(std::invalid_argument) {
+            return 0;
+        } catch(std::out_of_range) {
+            return 0;
+        }
+    }
+
     static option::ArgStatus Numeric(const option::Option& option, bool msg)
     {
         std::size_t pos = 0;
         if(option.arg) {
-            std::stoul(option.arg,&pos,10);
+            stoul(option.arg,&pos,10);
         }
         if(pos && !option.arg[pos]) {
             return option::ARG_OK;
@@ -32,15 +42,31 @@ struct Arg: public option::Arg
         return option::ARG_ILLEGAL;
     }
 
+    static option::ArgStatus Unsigned(const option::Option& option, bool msg)
+    {
+        std::size_t pos = 0;
+        if(option.arg) {
+            stoul(option.arg,&pos,10);
+        }
+        if(*option.arg!='-' && pos && !option.arg[pos]) {
+            return option::ARG_OK;
+        }
+
+        if (msg) {
+            fmt::print(stderr,"Option '{}' requires an unsigned argument\n",option);
+        }
+        return option::ARG_ILLEGAL;
+    }
+
     static option::ArgStatus Fraction(const option::Option& option, bool msg)
     {
         std::size_t pos = 0;
         if(option.arg) {
-            std::stoul(option.arg,&pos,0);
+            stoul(option.arg,&pos,0);
         }
         if(pos && option.arg[pos]=='/') {
             std::size_t pos2 = 0;
-            std::stoul(option.arg+pos+1,&pos2,0);
+            stoul(option.arg+pos+1,&pos2,0);
             if(pos2 && !option.arg[pos+1+pos2]) {
                 return option::ARG_OK;
             }
